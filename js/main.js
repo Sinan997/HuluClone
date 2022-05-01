e62540c16a45c567c5f4105b3bd008113dadf032
Sinan997 <sinanoz3437@gmail.com>
2022-05-01 18:30:35 +0300

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const modal = document.querySelector(".modal");
+const loginBtn = document.querySelector(".login-btn");
+const closeBtn = document.querySelector(".close");
+
+
+loginBtn.addEventListener("click", openModal)
+closeBtn.addEventListener("click", closeModal)
+window.addEventListener("click", outsideClick)
+
+function openModal() {
+    modal.style.display = "block";
+}
+
+function closeModal() {
+    modal.style.display = "none";
+}
+
+function outsideClick(e) {
+    if (e.target == modal) {
+        console.log(e.target);
+        closeModal();
+    }
+}
